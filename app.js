11f8ea1660802ca9e4a671093be02d6f6274c593
Miguel Cruz <miguel@miguelcruz.net>
2020-12-30 20:28:33 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const port = 3001
+
+const data = [
+    {
+      area: 'backend',
+      level: 'basic',
+      title: 'backend 101',
+      accomplished: 'true'
+    },
+    {
+      area: 'frontend',
+      level: 'basic',
+      title: 'frontend 101',
+      accomplished: 'false'
+    }
+  ]
+
+  app.get('/', (req, res) => {
+    res.send()
+})
+
+app.get('/skills', (req, res) => {
+    res.send(data)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
