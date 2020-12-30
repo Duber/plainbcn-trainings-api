fb0d73f3be553f981ba5124d0e2843ce9b750fc9
Miguel Cruz <miguel@miguelcruz.net>
2020-12-30 21:25:06 +0100

    reading skills from airtable

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 const express = require('express')
+const fetch = require("node-fetch");
+
 const app = express()
 const port = 3001
 
 const data = [
     {
-      area: 'backend',
-      level: 'basic',
-      title: 'backend 101',
-      accomplished: true
+        area: 'backend',
+        level: 'basic',
+        title: 'backend 101',
+        accomplished: true
     },
     {
-      area: 'frontend',
-      level: 'basic',
-      title: 'frontend 101',
-      accomplished: false
+        area: 'frontend',
+        level: 'basic',
+        title: 'frontend 101',
+        accomplished: false
     }
-  ]
+]
 
-  app.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send()
 })
 
-app.get('/api/skills', (req, res) => {
+app.get('/api/skills', async (req, res) => {
+    const url = `https://api.airtable.com/v0/appIJ1OyA5ly2fcib/Skills?api_key=${process.env.AIRTABLE_KEY}`
+    let data = await fetch(url).then((result) => result.json())
+    data = data.records.map((record)=>{
+        return {
+            id: record.id,
+            area: record.fields.Area,
+            level: record.fields.Level,
+            title: record.fields.Name,
+            accomplished: false
+        }
+    })
     res.json(data)
 })
 
