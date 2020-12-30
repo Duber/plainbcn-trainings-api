e6b702e59e8bf06b72108dd8a47077e04fc9fc4e
Miguel Cruz <miguel@miguelcruz.net>
2020-12-30 21:27:30 +0100

    deleted mock data

--- app.js
+++ app.js
@@ -7,21 +7,6 @@ const fetch = require("node-fetch");
 const app = express()
 const port = 3001
 
-const data = [
-    {
-        area: 'backend',
-        level: 'basic',
-        title: 'backend 101',
-        accomplished: true
-    },
-    {
-        area: 'frontend',
-        level: 'basic',
-        title: 'frontend 101',
-        accomplished: false
-    }
-]
-
 app.get('/', (req, res) => {
     res.send()
 })
