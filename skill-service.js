fa1473687f64aab4f89b163a4525121db9bbcb42
Miguel Cruz <miguel@miguelcruz.net>
2021-01-03 21:30:16 +0100

    airtable skills url env var

--- skill-service.js
+++ skill-service.js
@@ -6,7 +6,7 @@ const SKILLS_CACHE_DURATION = 300000
 
 class SkillService {
     async getAll() {
-        const airtable_skill_url = `https://api.airtable.com/v0/appIJ1OyA5ly2fcib/Skills?api_key=${process.env.AIRTABLE_KEY}`
+        const airtable_skill_url = `${process.env.AIRTABLE_SKILLS_URL}?api_key=${process.env.AIRTABLE_KEY}`
         let skills = cache.get(SKILLS_CACHE_KEY)
         if (!skills) {
             skills = await fetch(airtable_skill_url).then((result) => result.json())
