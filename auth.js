72c3b5c5bafb804b586740a71ae176b069a436ad
Miguel Cruz <miguel@miguelcruz.net>
2021-01-03 02:57:04 +0100

    removed passport dependecy by implementing custom token validation

---
+++ auth.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken'
+import DownloadPublicKey from './public-key-downloader.js'
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '')
+        const key = await getKey(token)
+        const decodedtoken = jwt.verify(token, key)
+        console.log(decodedtoken)
+        req.user = { email: decodedtoken.preferred_username }
+        next()
+    } catch (error) {
+        console.error(error)
+        res.status(401).send({ error: 'Not authorized to access this resource' })
+    }
+}
+
+function getKey(token){
+    const decodedToken = jwt.decode(token, { complete: true })
+    const keysUrl = `https://login.microsoftonline.com/${decodedToken.payload.tid}/discovery/v2.0/keys`
+    return DownloadPublicKey(keysUrl, decodedToken.header.kid)
+}
+
+export default auth
--- server.js
+++ server.js
@@ -2,15 +2,14 @@ import cors from 'cors'
 import express from 'express'
 import fetch from 'node-fetch'
 import dotenv from 'dotenv'
-import passport from 'passport'
-import * as passportAzureAd from 'passport-azure-ad'
+import auth from './auth.js'
 
 if (process.env.NODE_ENV !== 'production') {
     dotenv.config();
 }
 
 const server = express()
-const port = 3001
+const port = process.env.PORT || 3001
 
 server.use(cors())
 
@@ -18,22 +17,9 @@ server.get('/', (req, res) => {
     res.send()
 })
 
-server.use(passport.initialize());
-var options = {
-    identityMetadata: "https://login.microsoftonline.com/8c4daf01-5412-4589-86ff-f5efa0c2834f/v2.0/.well-known/openid-configuration",
-    clientID: "d3e21b0d-1460-45e7-8854-32333791077c",
-    issuer: "https://login.microsoftonline.com/8c4daf01-5412-4589-86ff-f5efa0c2834f/v2.0",
-    loggingLevel: "error",
-    audience: "24ebf232-d6f5-4e7b-bacd-e95ea35fca58",
-    passReqToCallback: false
-};
-var bearerStrategy = new passportAzureAd.BearerStrategy(options, function (token, done) {
-    done(null, { email: token.preferred_username }, token);
-});
-passport.use(bearerStrategy);
+server.use(auth)
 
-
-server.get('/api/skills', passport.authenticate("oauth-bearer", { session: false }), async (req, res) => {
+server.get('/api/skills', async (req, res) => {
     console.log("User info: ", req.user);
     const url = `https://api.airtable.com/v0/appIJ1OyA5ly2fcib/Skills?api_key=${process.env.AIRTABLE_KEY}`
     let data = await fetch(url).then((result) => result.json())
