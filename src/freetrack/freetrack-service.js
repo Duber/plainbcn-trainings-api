b3b3e1eae72cb8b8117763ecf5519592fbf949df
Miguel Cruz <miguel@miguelcruz.net>
2021-01-09 16:25:32 +0100

    freetrack-service default values

--- src/freetrack/freetrack-service.js
+++ src/freetrack/freetrack-service.js
@@ -13,13 +13,13 @@ class FreeTrackService {
             data = data.records.map((record) => {
                 return {
                     id: record.id,
-                    area: record.fields.Area,
-                    level: record.fields.Level,
-                    title: record.fields.Title,
-                    type: record.fields.Type,
-                    owners: record.fields['Owner(Name)'],
-                    notes: record.fields.Notes,
-                    scheduled: record.fields.Scheduled,
+                    area: record.fields.Area ?? '',
+                    level: record.fields.Level ?? '',
+                    title: record.fields.Title ?? '',
+                    type: record.fields.Type ?? '',
+                    owners: record.fields['Owner(Name)'] ?? [],
+                    notes: record.fields.Notes ?? null,
+                    scheduled: record.fields.Scheduled ?? null,
                     likes: (record.fields.Likes ?? []).length
                 }
             })
