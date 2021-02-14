3b4c993a232908adc0c25925dd3f17ffa28c4278
Miguel Cruz <miguel@miguelcruz.net>
2021-02-14 18:34:00 +0100

    people-service cache users individually to optimize cache invalidation

--- src/people/people-service.js
+++ src/people/people-service.js
@@ -2,24 +2,20 @@ import fetch from 'node-fetch'
 import cache from 'memory-cache'
 import PeopleMapper from './people-mapper.js'
 
-const CACHE_KEY = 'people'
+const CACHE_KEY_PREFIX = 'people'
 const CACHE_DURATION = 300000
 
 class PeopleService {
-    async getAll() {
-        const url = `${process.env.AIRTABLE_PEOPLE_URL}?api_key=${process.env.AIRTABLE_KEY}`
-        let data = cache.get(CACHE_KEY)
-        if (!data) {
-            data = await fetch(url).then((result) => result.json())
-            data = data.records.map(PeopleMapper.Map)
-            cache.put(CACHE_KEY, data, CACHE_DURATION)
-        }
-        return data
-    }
-
     async get(email) {
-        const data = await this.getAll()
-        return data.find(i => i.email === email)
+        const cacheKey = `${CACHE_KEY_PREFIX}_${email}`
+        let user = cache.get(cacheKey)
+        if (!user) {
+            const url = `${process.env.AIRTABLE_PEOPLE_URL}?api_key=${process.env.AIRTABLE_KEY}&filterByFormula={Email}="${email}"`
+            user = await fetch(url).then((result) => result.json())
+            user = user.records.map(PeopleMapper.Map)[0]
+            cache.put(cacheKey, user, CACHE_DURATION)
+        }
+        return user
     }
 
     async patch(id, data) {
@@ -42,7 +38,7 @@ class PeopleService {
         }
         const res = await fetch(url, options)
         if (!res.ok) throw new Error(`PeopleService.Patch for id ${id} and data ${JSON.stringify(data)} failed with status: ${res.status}:${res.statusText}`)
-        cache.del(CACHE_KEY)
+        cache.del(`${CACHE_KEY_PREFIX}_${data.email}`)
     }
 }
 
